505fdd74e6ab134919b4fa84e28c15d3266d6c77
OVSigurds <94639363+OVSigurds@users.noreply.github.com>
2021-12-10 20:48:13 +0000

    Add files via upload

---
+++ Python/100Days-Tip_Calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill?\n"))
+people = int(input("How many people will be sharing the bill?\n"))
+tip = int(input("How much of a tip would you like to give? [Popular tips = 10 / 12 / 15%]\n"))
+total = round(bill *(1 + tip/100), 2)
+share = round(total / people, 2)
+print('Each person should pay ${}, resulting in a total of ${}.'.format(share, total))
